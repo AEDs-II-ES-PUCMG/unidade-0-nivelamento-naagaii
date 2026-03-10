c220007e7e008812dc9d91d38edfa001d362bf2c
Francielle Limonge <fraanlimonge@gmail.com>
2026-03-10 12:32:07 -0300

    metodo GerarDadosTexto

--- src/ProdutoNaoPerecivel.java
+++ src/ProdutoNaoPerecivel.java
@@ -7,4 +7,10 @@ public class ProdutoNaoPerecivel extends Produto {
     public ProdutoNaoPerecivel (String desc, double precoCusto) {
 		super(desc, precoCusto);
 	}
+
+	@Override
+	public String gerarDadosTexto(){
+		return String.format(java.util.Locale.US, "1;%s;%.2f;%.2f", 
+                         this.descricao, this.precoCusto, this.margemLucro);
+	}
 }
--- src/ProdutoPerecivel.java
+++ src/ProdutoPerecivel.java
@@ -1,15 +1,16 @@
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
 public class ProdutoPerecivel extends Produto {
 
-    //constantes
+    // constantes
     private static final double DESCONTO = 0.25;
     private static final int PRAZO_DESCONTO = 7;
 
     private LocalDate dataDeValidade;
 
-    public ProdutoPerecivel (String desc, double precoCusto, double margemLucro, LocalDate dataDeValidade) {
+    public ProdutoPerecivel(String desc, double precoCusto, double margemLucro, LocalDate dataDeValidade) {
         super(desc, precoCusto, margemLucro);
         if (dataDeValidade.isBefore(LocalDate.now())) {
             throw new IllegalArgumentException("Data de validade não pode ser anterior ao dia atual.");
@@ -18,18 +19,26 @@ public class ProdutoPerecivel extends Produto {
         this.dataDeValidade = dataDeValidade;
     }
 
-    public double valorVenda (){
+    public double valorVenda() {
         double valorBase = super.valorDeVenda();
         long diasRestantes = ChronoUnit.DAYS.between(LocalDate.now(), this.dataDeValidade);
 
-        if(diasRestantes <= 0){
+        if (diasRestantes <= 0) {
             throw new IllegalArgumentException("Produto vencido.");
         }
-        if(diasRestantes <= PRAZO_DESCONTO){
+        if (diasRestantes <= PRAZO_DESCONTO) {
             valorBase = valorBase - (valorBase * DESCONTO);
         }
-        
+
         return valorBase;
 
+    }
+
+    @Override
+    public String gerarDadosTexto() {
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String dataTexto = this.dataDeValidade.format(formatador);
+        return String.format(java.util.Locale.US, "2;%s;%.2f;%.2f;%s", this.descricao, this.precoCusto,
+                this.margemLucro, dataTexto);
     }
 }
